712f7b7287be980d871717e6cf33ee66b844d31e
Fynn Emmanuel Junior <fynn.emmanuel.junior@gmail.com>
2023-12-15 01:45:43 +0000

    Update homepage navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,18 @@ import { useState,useEffect } from 'react'
 import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
 
 const Navbar = () => {
+	const isUserLogin = false
+	
+	const [providers,setProviders] = useState(null)
+
+	useEffect(() => {
+		const setProviders = async () => {
+			const response = await getProviders()
+			setProviders(response)
+		}
+
+		setProviders()
+	},[])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href='/' className="flex gap-2 flex-center">
@@ -14,9 +26,38 @@ const Navbar = () => {
                 alt='logo svg' 
                 width={30} 
                 height={30} 
-                className="object-contain"
+                className="object-contain" 
             />
+			<p className="logo_text"> Promptify </p>
         </Link>
+		
+		{/* Desktop navigation */}
+		<div className="sm:flex hidden">
+			{
+				isUserLogin ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href='/create-prompt' className="black_btn">
+							Create Post
+						</Link>
+						<button type="button" onClick={signOut} className="outline_btn">
+							Sign out 
+						</button>
+						<Link href='/profile'>
+							<Image 
+								src='/assets/images/logo.svg'
+								width={37}
+								height={37}
+								alt="profile image"
+								className="rounded-full"
+							/>
+						</Link>
+					</div>) : (
+					<> 
+
+					</>
+				)
+			}
+		</div>
     </nav>
   )
 }
