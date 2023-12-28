7928309ebec137986d6b7694a39f1c869c488c08
Fynn Emmanuel Junior <fynn.emmanuel.junior@gmail.com>
2023-12-28 22:54:08 +0000

    Updates on api

---
+++ api/controllers/usersController.ts
@@ -0,0 +1,9 @@
+import { Request,Response } from 'express';
+import  bcrypt  from 'bcrypt';
+import UserModel from '../models/UserModel'
+import jwt from 'jsonwebtoken'
+
+
+export const registerController = (req:Request,res:Response) => {
+
+}
--- api/routes/usersRoutes.ts
+++ api/routes/usersRoutes.ts
@@ -2,5 +2,7 @@ import express from 'express'
 
 const router = express.Router();
 
+router.get('/create-user',registerController)
+
 
 export default router
